eaf820c0825e2d92a3eae1e43e6946b8db4d9747
akshita0705-stack <raipurpramila@gmail.com>
2025-11-22 12:21:39 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 import sqlite3    #python's bulitin database
 
-# Create / connect to database
+# connecting to database
 conn = sqlite3.connect("events.db")
 cursor = conn.cursor()
 
-# Create table if not exists
+#This creates table if not exists
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,3 +16,4 @@ CREATE TABLE IF NOT EXISTS events (
 """)
 
 conn.commit()
+
